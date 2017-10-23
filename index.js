a2e6cfed69e4b22258e46d69a99f3c776283e148
Lawrence Aberba <karabutaworld@gmail.com>
2017-10-23 19:27:24 +0000

    Genesis

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MobileMoneyConfig } = require("./src/config");
-const MobileMoney = require("./src/mobile-money");
+const { MobileMoneyConfig } = require("src/config");
+const MobileMoney = require("src/mobile-money");
 
 module.exports = { MobileMoneyConfig, MobileMoney };
