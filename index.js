89667a918b54506ee667da068208ceb08698c0a2
Lawrence Aberba <karabutaworld@gmail.com>
2018-11-03 20:54:02 +0000

    add Config import

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { MobileMoneyConfig, SMSMessageConfig } = require("./src/config");
+const { MobileMoneyConfig, SMSMessageConfig, Config } = require("./src/config");
 const MobileMoney = require("./src/mobile-money");
 const SMSMessage = require("./src/sms-message");
 const util = require("./src/util");
 
 module.exports = {
+    Config,
+
     MobileMoneyConfig,
     MobileMoney,
 
